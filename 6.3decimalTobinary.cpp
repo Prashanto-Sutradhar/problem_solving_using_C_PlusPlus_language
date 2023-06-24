e73e4ba92533d5904ddaec3d6663d86abae3587e
Prashanto Sutradhar <121312298+prashanto777@users.noreply.github.com>
2023-06-24 18:58:03 +0600

    Add files via upload

---
+++ 6.3decimalTobinary.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int decimalTobinary(int n){
+
+  int ans =0;
+  int x =1;
+  while(x<=n)
+    x=x*2;
+  x=x/2;
+
+  while(x>0){
+    int lastdigit =n/x;
+    n= n-lastdigit*x;
+
+    x=x/2;
+    ans= ans*10 + lastdigit;
+  }
+  return ans;
+
+
+}
+
+
+
+int main(){
+   int n;
+   cin>>n;
+
+   cout<<decimalTobinary(n)<<endl;
+
+return 0;
+}
---
+++ primeBetweentwoNum.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+
+    cin>>a>>b;
+    int num;
+    int i;
+
+    for( num=a; num<=b; num++)
+    {
+        int i;
+        for( i=2; i<num; i++)
+        {
+
+            if(num%i==0)
+            {
+
+                break;
+
+            }
+        }
+        if(i == num)
+        {
+            cout<<num<<endl;
+        }
+
+    }
+
+    return 0;
+}
+
